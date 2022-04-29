ac74e73ac06a23425fda5c8b8cda27a871302249
Oana Florescu <oanaflores@fb.com>
2022-04-28 19:19:32 -0700

    added dummy text

--- src/components/layout/sections/HeroSection.js
+++ src/components/layout/sections/HeroSection.js
@@ -9,9 +9,11 @@ function HeroSection() {
         <ContentWrapper>
           <TextWrapper>
             <img src="/images/logos/logo.svg" alt="logo" />
-            <Title>Hi people</Title>
+            <Title>Design <br />
+              and code React apps</Title>
             <Description>
-              Welcome to your new Gatsby site.
+              Don’t skip design. Learn design and code, by building real apps with
+              React and Swift. Complete courses about the best tools.
             </Description>
           </TextWrapper>
           <Link to="/page-2/"> Go to page 2 </Link> <br />
