5ef7958e46203fdf8d5e915396436ad1c855e91b
Oana Florescu <oanaflores@fb.com>
2022-04-30 21:00:30 -0700

    class names and filters

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -8,7 +8,7 @@ export default function PurchaseButton(props) {
     <Link to="/page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon"/>
           <Ring src="/images/icons/icon-ring.svg" />
         </IconWrapper>
         <TextWrapper>
@@ -37,9 +37,17 @@ const Wrapper = styled.div`
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
       0px 30px 60px rgba(23, 0, 102, 0.5),
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+      transform: translateY(-3px);
   }
+  *,
+  & {
   transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
-  transform: translateY(-3px);
+
+  .icon {
+    transform: scale(1.2);
+  }
+}
+
 `
 
 const TextWrapper = styled.div`
@@ -62,6 +70,11 @@ const IconWrapper = styled.div`
   justify-content: center;
   align-content: center;
   position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg);
+  }
+
 `
 const Icon = styled.img`
   width: 29px;
@@ -72,4 +85,7 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
