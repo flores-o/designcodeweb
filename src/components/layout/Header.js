8845cc627820cfe479b78acd88b2cf95916c4a4e
Oana Florescu <oanaflores@fb.com>
2022-05-01 08:33:24 -0700

    style menu bar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,24 +1,57 @@
 import React from "react"
 import { Link } from "gatsby"
-
-const menuData = [
-    { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
-    {
-        title: "Tutorials",
-        icon: "/images/icons/tutorials.svg",
-        link: "/tutorials",
-    },
-    { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
-]
+import styled from "styled-components"
+import { menuData } from "../data/menuData"
 
 export default function Header() {
     return (
         <>
-            {menuData.map((item, index) => (
-                <Link to={item.link} key={index}>
-                 <img src={item.icon} alt={item.title}  />
-                    {item.title} </Link>
-            ))}
+            <Wrapper>
+                <img src="/images/logos/logo.svg" />
+                <MenuWrapper>
+                    {menuData.map((item, index) => (
+                        <MenuItem>
+                            <Link to={item.link} key={index}>
+                                <img src={item.icon} alt={item.title} />
+                                {item.title} </Link>
+                        </MenuItem>
+                    ))}
+                </MenuWrapper>
+            </Wrapper>
         </>
     )
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+	grid-template-columns: 44px auto;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	padding: 0 30px;
+`
+
+const MenuWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: 30px;
+  grid-template-columns: repeat(5, auto);
+`
+
+const MenuItem = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  display: grid;
+  grid-template-columns: 24px auto;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  border-radius: 10px;
+  transition: 0.5s ease-out;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+   }
+   `
