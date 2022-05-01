631dd18ce5b571bffc3e319472f2540ad653e081
Oana Florescu <oanaflores@fb.com>
2022-04-30 17:24:31 -0700

    added purchase button

--- src/components/layout/sections/HeroSection.js
+++ src/components/layout/sections/HeroSection.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1, MediumText } from "../../styles/TextStyles"
 import { themes } from "../../styles/ColorStyles"
-
+import PurchaseButton from "../../buttons/PurchaseButton"
 function HeroSection() {
   return (
     <>
@@ -17,6 +17,7 @@ function HeroSection() {
               Don’t skip design. Learn design and code, by building real apps with
               React and Swift. Complete courses about the best tools.
             </Description>
+            <PurchaseButton />
           </TextWrapper>
           <Link to="/page-2/"> Go to page 2 </Link> <br />
         </ContentWrapper>
